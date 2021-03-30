a4d26d6aa1bec473859bbc0eb335c33603f8f1a2
Adithya <adddii45@gmail.com>
2021-03-30 23:58:45 +0530

    Fixed a few bugs in PatientMedicalHistoryForm

--- Healthcare/Patient/forms.py
+++ Healthcare/Patient/forms.py
@@ -3,8 +3,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from Patient.models import Account
 from django.forms import ModelForm
-from django.contrib.auth.forms forms
-from Patient.models import Patient_medical_history
+from django.contrib.auth.forms import forms
+#from Patient.models import Patient_medical_history
 
 
 class SignupForm(UserCreationForm):
@@ -19,16 +19,36 @@ class SignupForm(UserCreationForm):
 class PatientMedicalHistoryForm(forms.Form):
 
     Full_name = forms.CharField(label='Full name',max_length=100)
-    DOB = forms.DateField(initial=datetime.date.now, format=('%d-%m-%Y'))
+    DOB = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'],
+        widget=forms.DateInput(
+           attrs={
+                'type': 'date', 
+                'class': 'form_input',
+           },
+        ),
+    )
     Age = forms.IntegerField()
     Gender = forms.CharField(max_length=20)
     Height = forms.IntegerField(label='in cm')
-    Weight = forms.IntegerField(lablel='in kg')
+    Weight = forms.IntegerField(label='in kg')
     Blood_Group =  forms.CharField(max_length=5)
-    Alchohol_Consumption = forms.TextField()
-    Smoking_Habit = forms.TextField()
-    Drug_Allergies = forms.TextField()
-    Current_Medications = forms.TextField()
+    Alchohol_Consumption = forms.CharField(max_length=50)
+    Smoking_Habit = forms.CharField(max_length=50)
+    Drug_Allergies = forms.CharField(widget=forms.Textarea)
+    Current_Medications = forms.CharField(widget=forms.Textarea)
     class Meta:
-        model = Patient_medical_history
-        fields("Full_name", "DOB", "Age", "Gender", "Height", "Weight", "Blood_Group", "Alchohol_Consumption", "Smoking_Habit", "Drug_Allergies", "Current_Medications",)
+        # model = Patient_medical_history
+        fields = ("Full_name", "DOB", "Age", "Gender", "Height", "Weight", "Blood_Group", "Alchohol_Consumption", "Smoking_Habit", "Drug_Allergies", "Current_Medications",)
+        widgets = {
+            'text': forms.Textarea(attrs={'rows':5, 'cols':10}), #this is changeble.
+        }
+
+#date-time code for to insert into html template
+# {{ form.date.id_for_label }}
+# <script>
+#   $(function () {
+#     $("#{{ form.date.id_for_label }}").datetimepicker({
+#       format: 'd/m/Y H:i',
+#     });
+#   });
+# </script>
