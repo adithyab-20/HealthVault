1af025ce097ef688645d82263fe35783d0b7d1fb
Adithya <adddii45@gmail.com>
2021-04-01 21:05:54 +0530

    Added Ajax to Patient signup

--- Healthcare/Patient/views.py
+++ Healthcare/Patient/views.py
@@ -2,26 +2,29 @@ from django.shortcuts import render
 from django.contrib.auth import login, authenticate
 from Patient.forms import SignupForm
 from django.contrib import messages
+from jsonview.decorators import json_view
+from django.template.context_processors import csrf
 
-def Signup(request):
-    context = {}
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            form.save()
-            Patient_fullname = form.cleaned_data.get('full_name')
-            Patient_email = form.cleaned_data.get('email')
-            raw_password = form.cleaned_data.get('password1')
-            account = authenticate(email=Patient_email, password=raw_password)
-            login(request,account)
-            messages.success(request, f'Account created for {Patient_fullname}!')
 
-        else:
-            context['signup_form'] = form
-    else:
-        form = SignupForm()
-        context['signup_form'] = form
-    return render(request, 'Patient_Signup.html', context)
+# def Signup(request):
+#     context = {}
+#     if request.method == 'POST':
+#         form = SignupForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             Patient_fullname = form.cleaned_data.get('full_name')
+#             Patient_email = form.cleaned_data.get('email')
+#             raw_password = form.cleaned_data.get('password1')
+#             account = authenticate(email=Patient_email, password=raw_password)
+#             login(request,account)
+#             messages.success(request, f'Account created for {Patient_fullname}!')
+
+#         else:
+#             context['signup_form'] = form
+#     else:
+#         form = SignupForm()
+#         context['signup_form'] = form
+#     return render(request, 'Patient_Signup.html', context)
 
 def Login(request):
     if request.method == 'POST':
@@ -34,5 +37,21 @@ def Login(request):
     return render(request, 'Patient_Signup.html', {'form': form})
 
 
-def Pat(request):
-    return render(request, 'Patient_Signup.html')
+@json_view
+def Signup(request):
+    form = SignupForm(request.POST or None)
+    context = {}
+    if form.is_valid():
+        form.save()
+        Patient_fullname = form.cleaned_data.get('full_name')
+        Patient_email = form.cleaned_data.get('email')
+        raw_password = form.cleaned_data.get('password1')
+        account = authenticate(email=Patient_email, password=raw_password)
+        login(request,account)
+        return {'success' : True }
+
+    context.update(csrf(request))
+
+    form_html = render(request, 'Patient_Signup.html', context)
+    
+    return {'success': False, 'form_html': form_html}
