9514c6fb29929ee6b3ca72abda1805637724a204
Adithya <adddii45@gmail.com>
2021-05-05 17:14:12 +0530

    Added authentication for Doctor and modified Doctor Login Template

--- Healthcare/Doctor/admin.py
+++ Healthcare/Doctor/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from . import models 
+from Patient import models as Pmodels
+
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
@@ -9,7 +12,33 @@ class ColumbiaAsiaAdminArea(admin.AdminSite):
 
 columbia_asia = ColumbiaAsiaAdminArea(name='ColumbiaAsia')
 
-columbia_asia.register(models.Doctor_CA)
+columbia_asia.register(models.ColumbiaAsia_Doctor)
+
+
+
+
+
+class AccountAdminConfig(UserAdmin):
+    search_fields = ('email', 'full_name')
+    list_display = ('email','full_name', 'date_joined', 'last_login', 'is_admin', 'is_staff',)
+    list_filter = ('email', 'is_active', 'is_staff')
+    ordering = ('email',)
+    readonly_fields = ('date_joined', 'last_login')
+
+    fieldsets = (
+        (None, {'fields': ('email','password')}),
+         ('Personal', {'fields': ('full_name',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+       
+    )
+    
+    add_fieldsets = (
+        (None, {'classes': ('wide',),
+        'fields': ('email', 'full_name', 'password1', 'password2', 'is_active', 'is_staff', 'is_admin')
+        }),
+    )
+
+columbia_asia.register(Pmodels.Account, AccountAdminConfig)
 
 class ApolloAdminArea(admin.AdminSite):
     site_header = "Apollo Hospital Admin"
--- Healthcare/Doctor/urls.py
+++ Healthcare/Doctor/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
+app_name = "Doctor"
 
 urlpatterns = [
-    path("", views.Login, name="Login"),
+    path("", auth_views.LoginView.as_view(template_name='Doctor_login.html'), name='Login'),
     path("Diagnosis", views.Latest_Diagnosis, name="LatestDiagnosisForm"),
 ]
